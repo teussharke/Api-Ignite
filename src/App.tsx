b1918ad12814469276098f5dd000bbdf73c9b3d2
Mateus Oliveira <109149179+teussharke@users.noreply.github.com>
2023-03-17 11:24:37 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { ThemeProvider } from "styled-components"
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { TransactionsProvider } from './contexts/TransactionsContext'
+import { Transactions } from './pages/Transactions'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello Word</h1>
-    </ThemeProvider>
+      <GlobalStyle />
 
+      <TransactionsProvider>
+        <Transactions />
+      </TransactionsProvider>
+    </ThemeProvider>
   )
 }
-
-
